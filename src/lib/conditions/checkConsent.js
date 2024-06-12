f6e0a462074ded27298707bc0fbbbb8b898de2bc
Patrick Hegnauer <patrick.hegnauer@bluewin.ch>
2024-06-12 12:50:04 +0200

    v2.2.8

--- src/lib/conditions/checkConsent.js
+++ src/lib/conditions/checkConsent.js
@@ -5,6 +5,27 @@ var cookieHelper = require('../../helpers/cookieHelper');
 module.exports = function(settings) {
   
   var extensionSettings = turbine.getExtensionSettings();
+  const botList = [
+    'googlebot',
+    'adsbot-google',
+    'applebot',
+    'bingbot',
+    'chrome-lighthouse',
+    'headlesschrome/',
+    'facebookexternalhit',
+    'google-read-aloud',
+    'baiuspider',
+    'cookiebot',
+    'petalbot'
+  ]
+  const userAgent = navigator.userAgent.toLowerCase();
+  const botFlag = false;
+
+  for (let elem of botList) {
+    if (userAgent.includes(elem)) {
+      botFlag = true
+    }
+  }
 
   if(_satellite.cookie.get(extensionSettings.cookieNameServer) !== undefined){
     var cookie = JSON.parse(_satellite.cookie.get(extensionSettings.cookieNameServer)).value;
@@ -17,13 +38,13 @@ module.exports = function(settings) {
   }
     
   
-  if(cookie === 'true'){
+  if(cookie === 'true' && botFlag !== true){
     return true;
   }
   else{
     //case marketing tag
     if(settings.marketing && !settings.stats){
-      if(cookie === 'marketing'){
+      if(cookie === 'marketing' && botFlag !== true){
       return true;
       }
       return false;
@@ -31,7 +52,7 @@ module.exports = function(settings) {
 
     //case stats tag
     if(settings.stats && !settings.marketing){
-    if(cookie === 'stats'){
+    if(cookie === 'stats' && botFlag !== true){
       return true;
     }
     return false;
@@ -39,7 +60,7 @@ module.exports = function(settings) {
 
     //case marketing & stats tag
     if(settings.marketing && settings.stats){
-    if(cookie === 'stats' || cookie === 'marketing'){
+    if((cookie === 'stats' || cookie === 'marketing') && botFlag !== true){
       return true;
     }
     return false;
